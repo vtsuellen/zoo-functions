e21dd1461a4c0dff2fc2fdfb0110902e0686cfc1
vtsuellen <vtsuellen@hotmail.com>
2023-05-08 16:04:24 -0300

    erros ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,7 +4,7 @@ const data = require('../data/zoo_data');
 const getSpeciesByIds = (...ids) => {
   if (ids.length === 0) {
     return [];
-  } 
+  }
   return ids.map((id) => species.find((specie) => specie.id === id));
 };
 
