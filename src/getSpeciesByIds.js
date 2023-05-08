95be9bfb6ef82419139dac82a7213d59ca6bff86
vtsuellen <vtsuellen@hotmail.com>
2023-05-08 15:59:24 -0300

    requirement 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,20 @@
-const data = require('../data/zoo_data');
+const { species } = require("../data/zoo_data");
+const data = require("../data/zoo_data");
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0) {
+    return [];
+  } else {
+    return ids.map((id) => {
+      return species.find((specie) => {
+        return specie.id === id;
+      });
+    });
+  }
 };
 
 module.exports = getSpeciesByIds;
+
+// map manipular e construir array
+// find encontrar o primeiro elemento que atende a uma determinada condição
+// ... Spread Operator nos permite copiar rapidamente todo ou parte de um array ou objeto existente para outro array ou objeto
