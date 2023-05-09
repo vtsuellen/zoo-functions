3c3fd50b2ef85407ced5c16296bd4d5c04b5c608
vtsuellen <vtsuellen@hotmail.com>
2023-05-09 13:13:27 -0300

    requirement 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const animals = species.find((specie) => specie.name === animal);
+  // retorna true se algum eleemento satisfaz uma condição 
+  return animals.residents.every((resident) => resident.age >= age);
 };
 
 module.exports = getAnimalsOlderThan;
