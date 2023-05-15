0cb88bca0b90558a5ce7a1fd4252667c4e1b29ca
vtsuellen <vtsuellen@hotmail.com>
2023-05-15 11:00:47 -0300

    requirement 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,6 +1,20 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-};
+  const resultado = {};
+  if (!animal) {
+    data.species.forEach((specie) => {
+      resultado[specie.name] = specie.residents.length;
+    });
+    return resultado;
+  }
+  const encontraSpecie = species.find((specie) => specie.name === animal.species);
+  const encontraBySex = encontraSpecie.residents
+    .filter((elemento) => elemento.sex === animal.sex);
 
+  if (animal.sex) return encontraBySex.length;
+  return encontraSpecie.residents.length;
+};
+console.log(countAnimals({ species: 'giraffes'}));
 module.exports = countAnimals;
