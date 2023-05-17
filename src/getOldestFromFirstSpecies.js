6ac1d574d613492da5338ac5eb351715d36b0888
vtsuellen <vtsuellen@hotmail.com>
2023-05-17 11:03:15 -0300

    requirement 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
-};
+const { employees, species } = data;
 
+const getOldestFromFirstSpecies = (id) => {
+  // encontra o id referente a pessoa colaboradora
+  const findId = employees.find((employee) => employee.id === id);
+  const findSpecie = species.find((specie) => specie.id === findId.responsibleFor[0]);
+  // array do animal mias velho
+  const animal = findSpecie.residents.reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+  return Object.values(animal);
+};
 module.exports = getOldestFromFirstSpecies;
